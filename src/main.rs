ee445163fdb046e72ce005c0d3ccc8980868bd90
cipepser <respepic@gmail.com>
2019-06-13 07:31:24 +0900

    write sample1

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use tokio::prelude::*;
+
+struct FutureTest {}
+
+impl Future for FutureTest {
+    type Item = ();
+    type Error = ();
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        println!("poll called!");
+        Ok(Async::Ready(()))
+    }
+}
+
+fn main() {
+    let f = FutureTest {};
+
+    tokio::run(f)
+}
