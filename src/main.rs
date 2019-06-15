a4889721796250f9e56cecfc880514aaaa4b15ee
cipepser <respepic@gmail.com>
2019-06-15 16:29:29 +0900

    implement a sample for NotReady

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 use tokio::prelude::*;
+use std::thread;
+use std::time;
 
-struct FutureTest {}
+struct FutureTest {
+    pool_count: i32,
+}
 
 impl Future for FutureTest {
     type Item = ();
     type Error = ();
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         println!("poll called!");
-        Ok(Async::Ready(()))
+        self.pool_count += 1;
+
+        let task = task::current();
+        thread::spawn(move || {
+            thread::sleep(time::Duration::from_millis(1000));
+            task.notify();
+        });
+
+        if self.pool_count <= 1 {
+            println!("not ready");
+            Ok(Async::NotReady)
+        } else {
+            println!("ready!");
+            Ok(Async::Ready(()))
+        }
     }
 }
 
 fn main() {
-    let f = FutureTest {};
+    let f = FutureTest {
+        pool_count: 0,
+    };
 
     tokio::run(f)
 }
